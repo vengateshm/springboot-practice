78000c660da1be6909ef1cbdc255d114e4461545
Vengatesh M <40466166+vengateshm@users.noreply.github.com>
2024-05-08 16:23:03 +0530

    scheduling with fixedRate, fixedDelay, initialDelay, cron

---
+++ src/main/java/dev/vengateshm/springboot_practice/scheduling/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package dev.vengateshm.springboot_practice.scheduling;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
---
+++ src/main/java/dev/vengateshm/springboot_practice/scheduling/SchedulingService.java
@@ -0,0 +1,21 @@
+package dev.vengateshm.springboot_practice.scheduling;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class SchedulingService {
+//        @Scheduled(fixedRate = 3_000)
+//    @Scheduled(fixedDelay = 3_000, initialDelay = 5_000)
+@Scheduled(cron = "0 22 16 * * MON-FRI")
+    public void fixedRateScheduling() {
+        log.info("Hi");
+//        try {
+//            Thread.sleep(6000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+    }
+}
